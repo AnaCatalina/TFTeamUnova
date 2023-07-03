c59395b81a310021dacaf6be8d2e3cefef41bd6d
EliasDRobles <93445911+EliasDRobles@users.noreply.github.com>
2023-07-03 16:45:49 -0300

    Nivel 1 y 2
    Corregido Nivel 1 y Actualizado Nivel 2
    
    Co-Authored-By: estradalautaro <93290605+estradalautaro@users.noreply.github.com>
    Co-Authored-By: fab1997 <93455997+fab1997@users.noreply.github.com>
    Co-Authored-By: BoccardoAnaCatalina <93290449+AnaCatalina@users.noreply.github.com>

--- Assets/Script/Cable.cs
+++ Assets/Script/Cable.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Cable : MonoBehaviour
 {
+    [SerializeField]
+    private int nivel;
     [SerializeField]
     private float posX;
     [SerializeField]
@@ -13,8 +16,30 @@ public class Cable : MonoBehaviour
     private float posZ;
     // Start is called before the first frame update
     [SerializeField]
-    private float angle;
+    private float angulo;
 
+    private void Start()
+    {
+        posY = 0.5f;
+        switch (nivel)
+        {
+            case 1:
+                posX = -63.5f;
+                posZ = 87f;
+                angulo = 180f;
+                break;
+            case 2:
+                posX = 110f;
+                posZ = 60f;
+                angulo = 180f;
+                break;
+            case 3:
+                posX = 195f;
+                posZ = -80f;
+                angulo = 270f;
+                break;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -23,13 +48,14 @@ public class Cable : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //PlayerBHVR player = GameObject.Find("Dog_D_Casual_A").GetComponent<PlayerBHVR>();
 
+        //MovJugador jugador = GameObject.Find("Dog_D_Casual_A").GetComponent<MovJugador>();
         if (other.gameObject.CompareTag("Jugador"))
         {
+            
             //other.transform.position = new Vector3(63, 0.5f, 89);
-            other.transform.position = new Vector3(posX, posY, posZ);//Devuelve al personaje al principio
-            other.transform.localRotation = Quaternion.Euler(0, angle, 0);
+            other.transform.position = new Vector3(posX, posY , posZ);//Devuelve al personaje al principio
+            other.transform.localRotation = Quaternion.Euler(0, angulo, 0);
 
         }
     }
